382bd311614fa60c5276f27314fbddac11530c54
Angelica Quilme Manlapaz <77945056+Angelica2001143@users.noreply.github.com>
2021-04-09 16:31:25 +0800

    updated

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-const express = require ('express');
-const cors  = require  ('cors');
-const mongoose = require ('mongoose');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const UserRouter = require('./routes/user');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8080 ; //port
+const port  = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
-
-const uri = process.env.ATLAS_URI; //get connection string from 
-mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+ 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
-connection.once('open',() => {
-console.log('MongoDb connection established:');
-});
+connection.once('open', () => {
+    console.log('MongoDB connection established.');
+})
 
 app.use('/user', UserRouter);
 
-//run server
-app.listen(port, () =>{
-    console.log('SERVER IS RUNNING AT PORT : ' + port)
+app.listen(port, () => {
+    console.log('Server is runnning at port: ' + port);
 });
